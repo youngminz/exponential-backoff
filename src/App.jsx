a12317f5c1279fbcf8799b7a6c96739269c3038d
Youngmin Koo <youngminz.kr@gmail.com>
2020-02-26 00:14:06 +0900

    refactor: logic & naming cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,42 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const calculateBackoff = (interval, retryAttempt, backoffRate) => {
+const calculateBackoff = (
+  minRetryBackoff,
+  maxRetryBackoff,
+  totalRetryCount
+) => {
   let result = [];
-  let start = interval;
 
-  for (let i = 1; i <= retryAttempt; i++) {
-    result.push({ retryCount: i, seconds: start });
-    start = start * backoffRate + interval;
+  for (let retryCount = 1; retryCount <= totalRetryCount; retryCount++) {
+    result.push({
+      retryCount: retryCount,
+      seconds: Math.min(
+        minRetryBackoff * 2 ** (retryCount - 1),
+        maxRetryBackoff
+      )
+    });
   }
 
   return result;
 };
 
 const App = () => {
-  const [interval, setInterval_] = useState("2");
-  const [retryAttempt, setRetryAttempt] = useState("8");
-  const [backoffRate, setBackoffRate] = useState("2");
+  const [minRetryBackoff, setMinRetryBackoff] = useState(0.1);
+  const [maxRetryBackoff, setMaxRetryBackoff] = useState(60.0);
+  const [totalRetryCount, setTotalRetryCount] = useState(15);
 
   const [result, setResult] = useState([]);
 
-  const validation = () => {
-    if (isNaN(parseFloat(interval))) {
-      return false;
-    }
-
-    if (isNaN(parseInt(retryAttempt))) {
-      return false;
-    }
-
-    if (isNaN(parseFloat(backoffRate))) {
-      return false;
-    }
-
-    return true;
-  };
-
   useEffect(() => {
-    const validatedParams = validation();
-
-    // TODO: 반환 값을 두 개의 파라미터로 나누기
-    if (!validatedParams) {
-      return;
-    }
-
     setResult(
       calculateBackoff(
-        parseFloat(interval),
-        parseInt(retryAttempt),
-        parseFloat(backoffRate)
+        parseFloat(minRetryBackoff),
+        parseFloat(maxRetryBackoff),
+        parseInt(totalRetryCount)
       )
     );
-  }, [interval, retryAttempt, backoffRate]);
+  }, [minRetryBackoff, maxRetryBackoff, totalRetryCount]);
 
   return (
     <>
@@ -60,21 +45,21 @@ const App = () => {
       <div id="parameter">
         <input
           type="text"
-          value={interval}
-          placeholder="Interval (seconds)"
-          onChange={e => setInterval_(e.target.value)}
+          value={minRetryBackoff}
+          placeholder="{min retry backoff}"
+          onChange={e => setMinRetryBackoff(e.target.value)}
         />
         <input
           type="text"
-          value={retryAttempt}
-          placeholder="Retry attempt"
-          onChange={e => setRetryAttempt(e.target.value)}
+          value={maxRetryBackoff}
+          placeholder="{max retry backoff}"
+          onChange={e => setMaxRetryBackoff(e.target.value)}
         />
         <input
           type="text"
-          value={backoffRate}
-          placeholder="Backoff rate"
-          onChange={e => setBackoffRate(e.target.value)}
+          value={totalRetryCount}
+          placeholder="{total retry count}"
+          onChange={e => setTotalRetryCount(e.target.value)}
         />
       </div>
       <h2>Result</h2>
