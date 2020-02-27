871dc996439a62d96606ca77106598af5576ab98
Youngmin Koo <youngminz.kr@gmail.com>
2020-02-28 00:08:06 +0900

    Updates

--- precache-manifest.53c4aa8d598d73d4e50131236334d81e.js
+++ precache-manifest.53c4aa8d598d73d4e50131236334d81e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aedb8e82c08bd97e90a8511eeb10575c",
+    "revision": "ab5f6795f3f2b8bf2f2db4eb1ad9def5",
     "url": "/index.html"
   },
   {
-    "revision": "c2ef55cb12d28469bd8d",
-    "url": "/static/css/main.5c867702.chunk.css"
+    "revision": "5337bb1a5b7a88bcbe26",
+    "url": "/static/css/main.f0f693ee.chunk.css"
   },
   {
     "revision": "fb86d2373348db784429",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.047cdf02.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c2ef55cb12d28469bd8d",
-    "url": "/static/js/main.2aa0ad60.chunk.js"
+    "revision": "5337bb1a5b7a88bcbe26",
+    "url": "/static/js/main.1c259afd.chunk.js"
   },
   {
     "revision": "e8935ff216c127204006",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31a5413ad71efb6d003ae5ff9a193051.js"
+  "/precache-manifest.53c4aa8d598d73d4e50131236334d81e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
