b165db1242ebbfc2ca1654656cd924ac1df913db
Youngmin Koo <youngminz.kr@gmail.com>
2020-02-29 14:25:25 +0900

    feat: add description

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,15 @@ const Header = () => {
   return (
     <header className={cx("Header")}>
       <h1>truncated exponential backoff simulator</h1>
+      <p>
+        Truncated exponential backoff is a standard error handling strategy for
+        network applications in which a client periodically retries a failed
+        request with increasing delays between requests. (Ref:{" "}
+        <a href="https://cloud.google.com/storage/docs/exponential-backoff">
+          Google Cloud Documentation
+        </a>
+        )
+      </p>
     </header>
   );
 };
