bae3a7c811f5932a2518dfd473d5382b0e1558ff
Youngmin Koo <youngminz.kr@gmail.com>
2020-03-01 00:34:15 +0900

    feat: add chart view

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames/bind";
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+  ResponsiveContainer
+} from "recharts";
 
 import styles from "./Result.module.scss";
 const cx = classNames.bind(styles);
@@ -14,31 +24,76 @@ const formatTime = seconds => {
 };
 
 const Result = ({ calculationResult }) => {
-  const header = (
-    <tr>
-      <th>retry #</th>
-      <th>current backoff</th>
-      <th>accumulated backoff</th>
-    </tr>
+  const [mode, setMode] = useState("table");
+  const modeTransition = {
+    table: "chart",
+    chart: "table"
+  };
+
+  const toggleButton = (
+    <button
+      onClick={() => {
+        setMode(modeTransition[mode]);
+      }}
+    >
+      Toggle to {modeTransition[mode]} view
+    </button>
   );
-  const body = calculationResult.map(
-    ({ retryCount, backoffSeconds, accumulateBackoffSeconds }) => {
-      return (
-        <tr key={retryCount}>
-          <td>{retryCount}</td>
-          <td>{formatTime(backoffSeconds)}</td>
-          <td>{formatTime(accumulateBackoffSeconds)}</td>
-        </tr>
-      );
-    }
-  );
+
+  let content;
+
+  if (mode === "chart") {
+    content = (
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={calculationResult}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="retryCount" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="backoffSeconds" stroke="#8884d8" />
+          <Line
+            type="monotone"
+            dataKey="accumulateBackoffSeconds"
+            stroke="#82ca9d"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  } else {
+    const header = (
+      <tr>
+        <th>retry #</th>
+        <th>current backoff</th>
+        <th>accumulated backoff</th>
+      </tr>
+    );
+    const body = calculationResult.map(
+      ({ retryCount, backoffSeconds, accumulateBackoffSeconds }) => {
+        return (
+          <tr key={retryCount}>
+            <td>{retryCount}</td>
+            <td>{formatTime(backoffSeconds)}</td>
+            <td>{formatTime(accumulateBackoffSeconds)}</td>
+          </tr>
+        );
+      }
+    );
+
+    content = (
+      <div className={cx("table-wrapper")}>
+        <table>
+          <thead>{header}</thead>
+          <tbody>{body}</tbody>
+        </table>
+      </div>
+    );
+  }
 
   return (
     <div className={cx("Result")}>
-      <table>
-        <thead>{header}</thead>
-        <tbody>{body}</tbody>
-      </table>
+      {content}
+      {toggleButton}
     </div>
   );
 };
